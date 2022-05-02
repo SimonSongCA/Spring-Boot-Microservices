db85e4ff0c7006a2fe58a6855c07eddc82dc3035
Simon Song <65002336+SimonSongCA@users.noreply.github.com>
2022-05-02 12:26:34 -0700

    fix bug at ApiGatewayConfiguration

--- api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayConfiguration.java
@@ -17,12 +17,14 @@ public class ApiGatewayConfiguration {
                                  .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
                                                 .addRequestParameter("param", "myParam"))
                                  .uri("http://httpbin.org:80"))
+                  // Here, the value of 'uri' is the 'Application' name registered under Eureka.
+                  // Values of upper/lower cases are ignored.
                   .route(p -> p.path("/currency-exchange/**")
-                                .uri("lb://currency-exchange"))
+                               .uri("lb://currency-exchange"))
                   .route(p -> p.path("/currency-conversion/**")
                                .uri("lb://currency-conversion"))
                   .route(p -> p.path("/currency-conversion-feign/**")
-                               .uri("lb://currency-conversion-feign"))
+                               .uri("lb://currency-conversion"))
                   .build();
   }
 }
