38bf371899c0b8c9bde0a7356d7271b9f7dfcd4d
Simon Song <65002336+SimonSongCA@users.noreply.github.com>
2022-05-02 22:02:14 -0700

    Step 29 Rate Limiting & Bulkhead
    @Bulkhead: a limiter to limit the maximum number of concurrent requests
    
    @RateLimiter: a limiter to limit the maximum of requests within a specified period of time

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +19,21 @@ public class CircuitBreakerController {
   //  @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
   //  A circuit breaker. Doing this will allow Spring to return a body back without calling the
   //  method of the controller.
-  @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+  //  @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+  //  A rate limiter that limits how many requests are allowed within a period of time
+  @RateLimiter(name = "default-rate-limiter")
+  // @Bulkhead: A rate limiter that limits the maximum number concurrent calls
+  @Bulkhead(name = "bulkhead-rate-limiter")
   public String sampleApi() {
     
-    logger.info("Sample api call received.");
-    ResponseEntity<String> entity =
-            new RestTemplate().getForEntity("http://localhost:8080/dumb-shit",
-                                            String.class);
-    return entity.getBody();
+//        logger.info("Sample api call received.");
+//        ResponseEntity<String> entity =
+//                new RestTemplate().getForEntity("http://localhost:8080/dumb-shit",
+//                                                String.class);
+//        return entity.getBody();
+    
+    // a sample response under rate limiters
+    return "sample-api under rate limiter";
   }
   
   public String hardcodedResponse(Exception ex) {
